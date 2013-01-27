4ea1c4d93c1fe407fa2f62466a48068bba3d759c
k10 <ssjmetroid@hotmail.com>
2013-01-27 00:29:24 -0500

    I fucking did it.

--- js/room.js
+++ js/room.js
@@ -20,6 +20,7 @@ function Room(json) {
 	this.half_x = this.width*TILE_SIZE / 2;	
 	
 	this.processCollision();
+	this.processFGTiles();
 }
 
 Room.prototype.solid = function(y, x) {
@@ -89,6 +90,541 @@ Room.prototype.processCollision = function() {
 		}
 	}
 };
+
+Room.prototype.processFGTiles = function() {
+	//SAFETY!
+	var temp = new Array(this.height);
+	for(var i = 0; i < this.height; i++){
+		temp[i] = new Array(this.width);
+		for(var j = 0; j < this.width; j++){
+			temp[i][j] = this.fg[i][j];
+			if(this.isTileValid(this.fg[i][j])){
+				this.fg[i][j] = temp[i][j] = 0;
+			}
+		}
+	}
+	for(var r = 0; r < this.height; r++){
+		for(var c = 0; c < this.width; c++){
+			var test = false;//Use this just in case things get FUCKED...
+			if (!this.isTileValid(this.fg[r][c]))
+			{
+				continue;
+				if(test) console.out.log("ABORTED!");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 0;
+				if(test) console.log("SELECTED!0");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 1;
+				if(test) console.log("SELECTED!1");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 2;
+				if(test) console.log("SELECTED!2");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 3;
+				if(test) console.log("SELECTED!3");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 4;
+				if(test) console.log("SELECTED!4");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 5;
+				if(test) console.log("SELECTED!5");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 6;
+				if(test) console.log("SELECTED!6");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 7;
+				if(test) console.log("SELECTED!7");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 8 + 8;
+				if(test) console.log("SELECTED!8");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 9 + 8;
+				if(test) console.log("SELECTED!9");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 10 + 8;
+				if(test) console.log("SELECTED!10");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 11 + 8;
+				if(test) console.log("SELECTED!11");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 12 + 8;
+				if(test) console.log("SELECTED!12");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 13 + 8;
+				if(test) console.log("SELECTED!13");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 14 + 8;
+				if(test) console.log("SELECTED!14");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 15 + 8;
+				if(test) console.log("SELECTED!15");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 16 + 16;
+				if(test) console.log("SELECTED!16");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 17 + 16;
+				if(test) console.log("SELECTED!17");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 18 + 16;
+				if(test) console.log("SELECTED!18");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 19 + 16;
+				if(test) console.log("SELECTED!19");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 20 + 16;
+				if(test) console.log("SELECTED!19");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 21 + 16;
+				if(test) console.log("SELECTED!20");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 22 + 16;
+				if(test) console.log("SELECTED!21");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 23 + 16;
+				if(test) console.log("SELECTED!23");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 24 + 24;
+				if(test) console.log("SELECTED!24");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 25 + 24;
+				if(test) console.log("SELECTED!25");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 26 + 24;
+				if(test) console.log("SELECTED!26");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 27 + 24;
+				if(test) console.log("SELECTED!27");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 28 + 24;
+				if(test) console.log("SELECTED!28");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 29 + 24;
+				if(test) console.log("SELECTED!29");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 30 + 24;
+				if(test) console.log("SELECTED!30");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 31 + 24;
+				if(test) console.log("SELECTED!31");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 32 + 32;
+				if(test) console.log("SELECTED!32");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) == 1)
+			{
+				temp[r][c] = 33 + 32;
+				if(test) console.log("SELECTED!33");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c - 1) == 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 34 + 32;
+				if(test) console.log("SELECTED!34");
+			}
+			if (this.findTile(r - 1, c - 1) == 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 35 + 32;
+				if(test) console.log("SELECTED!35");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) == 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 36 + 32;
+				if(test) console.log("SELECTED!36");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 37 + 32;
+				if(test) console.log("SELECTED!37");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 38 + 32;
+				if(test) console.log("SELECTED!38");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 39 + 32;
+				if(test) console.log("SELECTED!39");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 40 + 40;
+				if(test) console.log("SELECTED!40");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) == 1 &&
+				this.findTile(r + 1, c + 1) != 1)
+			{
+				temp[r][c] = 41 + 40;
+				if(test) console.log("SELECTED!41");
+			}
+			if (this.findTile(r - 1, c) != 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c - 1) != 1 &&
+				this.findTile(r + 1, c) == 1)
+			{
+				temp[r][c] = 42 + 40;
+				if(test) console.log("SELECTED!42");
+			}
+			if (this.findTile(r - 1, c - 1) != 1 &&
+				this.findTile(r - 1, c) == 1 &&
+				this.findTile(r, c - 1) == 1 &&
+				this.findTile(r, c + 1) != 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 43 + 40;
+				if(test) console.log("SELECTED!43");
+			}
+			if (this.findTile(r - 1, c) == 1 &&
+				this.findTile(r - 1, c + 1) != 1 &&
+				this.findTile(r, c - 1) != 1 &&
+				this.findTile(r, c + 1) == 1 &&
+				this.findTile(r + 1, c) != 1)
+			{
+				temp[r][c] = 44 + 40;
+				if(test) console.log("SELECTED!44");
+			}
+			if(test) console.log("END SELECTING");
+		}
+	}
+	//SAFETY!
+	for(var i = 0; i < this.height; i++){
+		for(var j = 0; j < this.width; j++){
+			this.fg[i][j] = temp[i][j];
+		}
+	}
+}
+
+Room.prototype.wrapClampRow = function(row){
+	var delta = this.height + 1;
+	while (row < 0)
+	{
+		row += delta;
+	}
+	while (row > this.height)
+	{
+		row -= delta;
+	}
+	return row;
+}
+
+Room.prototype.wrapClampCol = function(col){
+	var delta = this.width + 1;
+	while (col < 0)
+	{
+		col += delta;
+	}
+	while (col > this.width)
+	{
+		col -= delta;
+	}
+	return col;
+}
+
+Room.prototype.findTile = function(row, col){
+	//FUCK IT.
+	var r = row;//this.wrapClampRow(row);
+	var c = col;//this.wrapClampCol(col);
+	if(r < 0 || r >= this.height){
+		return 1;
+	}
+	if(c < 0 || c >= this.width){
+		return 1;
+	}
+	return this.isTileValid(this.fg[r][c]) ? 1 : 0;
+}
+
+Room.prototype.isTileValid = function(id){
+	return (id >= 0 && id < 8) ||
+			(id >= 16 && id < 24) ||
+			(id >= 32 && id < 40) ||
+			(id >= 48 && id < 56) ||
+			(id >= 64 && id < 72) ||
+			(id >= 80 && id < 88);
+}
 
 Room.prototype.getFlagState = function(x, y, direction) {
 	if(this.collideFlags[y][x] | direction > 0)
