5873468b8df5139698f136c8b7abec5b4d6dbc1e
Miasmir <tehmakr@gmail.com>
2013-01-27 14:09:41 -0500

    new levels!!!!

--- js/levels.js
+++ js/levels.js
@@ -2,31 +2,41 @@ assetManager.levelQueue = [
 {
 	name: "PROVING GROUNDS",
 	source: "levels/mark3.json",
-	id: 0
+	id: 1
 },
 {
 	name: "BURNING BRIDGES",
 	source: "levels/mark1.json",
-	id: 1
+	id: 3
 },
 {
 	name: "BAPTISM OF FLAME",
 	source: "levels/mark2.json",
-	id: 2
+	id: 6
 },
 {
 	name: "High Ground",
 	source: "levels/intro1.json",
-	id: 3
+	id: 0
 },
 {
-	name: "TEST",
-	source: "levels/template.json",
+	name: "SIZZLING SEA",
+	source: "levels/mark4.json",
+	id: 2
+},
+{
+	name: "RUINED GLORY",
+	source: "levels/mark5.json",
 	id: 4
+},
+{
+	name: "VORTEX",
+	source: "levels/mark6.json",
+	id: 5
 },
 {
 	name: "Stay Afloat",
 	source: "levels/eugene0.json",
-	id: 5
+	id: 7
 }
 ]
